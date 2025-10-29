e128ef43d6a870d32012eeff92d63c8ffb22ab40
Anamika Bharti <anamika789ak@gmail.com>
2025-10-29 11:10:35 +0530

    build Fixes

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,34 @@
  const express = require("express");
 const app = express();
 const cors = require("cors");
-
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
-require('dotenv').config()
+require('dotenv').config();
 
 // middleware
 app.use(express.json());
 app.use(cors({
     origin: ['http://localhost:5173', 'https://book-store-app-pi-eight.vercel.app/'],
     credentials: true
-}))
+}));
 
-// routes
+// api routes
 const bookRoutes = require('./src/books/book.route');
-const orderRoutes = require("./src/orders/order.route")
-const userRoutes =  require("./src/users/user.route")
-const adminRoutes = require("./src/stats/admin.stats")
+const orderRoutes = require("./src/orders/order.route");
+const userRoutes =  require("./src/users/user.route");
+const adminRoutes = require("./src/stats/admin.stats");
 
-app.use("/api/books", bookRoutes)
-app.use("/api/orders", orderRoutes)
-app.use("/api/auth", userRoutes)
-app.use("/api/admin", adminRoutes)
+app.use("/api/books", bookRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/auth", userRoutes);
+app.use("/api/admin", adminRoutes);
 
+// Connect to MongoDB
 async function main() {
   await mongoose.connect(process.env.DB_URL);
-  app.use("/", (req, res) => {
-    res.send("Book Store Server is running!");
-  });
+  // THE BAD "/" ROUTE HAS BEEN REMOVED.
 }
 
 main().then(() => console.log("Mongodb connect successfully!")).catch(err => console.log(err));
+
 export default app;
